012da422a7e88ef6b194e1843b47935c764b84ea
AleIsPale435 <alebarr564@gmail.com>
2024-02-28 15:02:21 -0700

    AllMigrations

--- Portfolio/portfolio_app/models.py
+++ Portfolio/portfolio_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    contact_email = models.CharField(max_length=200, null=True)
+    is_active = models.BooleanField()
+    about = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('portfolio-detail', args=[str(self.id)])
+
+class Student(models.Model):
+
+#List of choices for major value in database, human readable name
+    MAJOR = (
+    ('CSCI-BS', 'BS in Computer Science'),
+    ('CPEN-BS', 'BS in Computer Engineering'),
+    ('BIGD-BI', 'BI in Game Design and Development'),
+    ('BICS-BI', 'BI in Computer Science'),
+    ('BISC-BI', 'BI in Computer Security'),
+    ('CSCI-BA', 'BA in Computer Science'),
+    ('DASE-BS', 'BS in Data Analytics and Systems Engineering')
+)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField("UCCS Email", max_length=200, null=True)
+    major = models.CharField(max_length=200, choices=MAJOR, blank = False, null=True)
+    portfolio = models.ForeignKey(Portfolio, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('student-detail', args=[str(self.id)])
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(blank=False, null=True)
+    portfolio = models.ForeignKey(Portfolio, null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('project-detail', args=[str(self.id)])
+
