edd63580c31b4520444ad6264f83b25808ba9f01
AleIsPale435 <alebarr564@gmail.com>
2024-02-28 18:43:16 -0700

    Database

--- Portfolio/portfolio_app/models.py
+++ Portfolio/portfolio_app/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Portfolio(models.Model):
     title = models.CharField(max_length=200, null=True)
     contact_email = models.CharField(max_length=200, null=True)
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=False)
     about = models.TextField(null=True, blank=True)
 
     def __str__(self):
@@ -30,7 +30,7 @@ class Student(models.Model):
     name = models.CharField(max_length=200, null=True)
     email = models.CharField("UCCS Email", max_length=200, null=True)
     major = models.CharField(max_length=200, choices=MAJOR, blank = False, null=True)
-    portfolio = models.ForeignKey(Portfolio, null=True, on_delete=models.SET_NULL)
+    portfolio = models.ForeignKey(Portfolio, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -42,7 +42,7 @@ class Student(models.Model):
 class Project(models.Model):
     title = models.CharField(max_length=200, null=True)
     description = models.TextField(blank=False, null=True)
-    portfolio = models.ForeignKey(Portfolio, null=True, on_delete=models.SET_NULL)
+    portfolio = models.ForeignKey(Portfolio, null=True, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
